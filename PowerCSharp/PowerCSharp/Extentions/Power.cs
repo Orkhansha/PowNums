db0db87879a771646ebd02f03e8828162e176829
Orkhansha <orkhansha@code.edu.az>
2022-07-01 22:18:43 +0400

    Added PowNums programs files

---
+++ PowerCSharp/PowerCSharp/Extentions/Power.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerCSharp.Extentions
+{
+    public static class Power
+    {
+        public static double PowNums(this int num1, int num2)
+        {
+            return Math.Pow(num1, num2);
+        }
+    }
+}
---
+++ PowerCSharp/PowerCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using PowerCSharp.Extentions;
+using System;
+
+namespace PowerCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 3;
+            var result = num1.PowNums(2);
+            Console.WriteLine(result);
+
+        }
+    }
+}
